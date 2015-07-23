72816091a244472bc2cb9e8ec46b7f615ca97d8e
jason cruz <jason.cruz@xtv-inc.com>
2015-07-22 17:06:51 -0700

    added deletion feature

--- client/app.js
+++ client/app.js
@@ -3,29 +3,47 @@ var app = angular.module("dataStruct", []);
 
 app.controller("linkedListCtrl", function($scope) {
 
-	$scope.link = new linkList();
-	$scope.newNode = null;
-	$scope.linkArray = [];
+	$scope.link = new LinkList();
+	$scope.addEndNode = null;
+	$scope.addBeginningNode = null;
+	$scope.linkArray;
 
-	var getArray = function() {
+	var toArray = function() {
 		var current = $scope.link.start;
+		var newArray = [];
 		while(current !== null) {
-			$scope.linkArray.push(current.data);
+			newArray.push(current.data);
 			current = current.next;
 		}
+		return newArray;
 	}
 
 	$scope.add = function() {
-
-		if($scope.newNode) {
-			var data = $scope.newNode;
+		if($scope.addEndNode) {
+			var data = $scope.addEndNode;
 			$scope.link.add(data);
 			$scope.linkArray.push(data);
-			$scope.newNode = null;
+			$scope.addEndNode = null;
 		} else {
 			console.log('error');
 		}
 	}
 
-	getArray();
+	$scope.addToFront = function() {
+		if($scope.addBeginningNode) {
+			var data = $scope.addBeginningNode;
+			$scope.link.addAtFirst(data);
+			$scope.linkArray.unshift(data);
+			$scope.addBeginningNode = null;
+		} else {
+			console.log('error');
+		}
+	}
+
+	$scope.delete = function(n) {
+		$scope.link.delete(n);
+		$scope.linkArray = toArray();
+	}
+
+	$scope.linkArray = toArray();
 });
--- client/js/linkedList.js
+++ client/js/linkedList.js
@@ -1,6 +1,6 @@
-var linkList = function() {
+var LinkList = function() {
 
-	linkList.makeNode = function() {
+	LinkList.makeNode = function() {
 		return {
 			data : null,
 			next : null
@@ -12,13 +12,59 @@ var linkList = function() {
 
 	this.add = function(data) {
 		if(this.start === null) {
-			this.start = linkList.makeNode();
+			this.start = LinkList.makeNode();
 			this.end = this.start;
 		} else {
-			this.end.next = linkList.makeNode();
+			this.end.next = LinkList.makeNode();
 			this.end = this.end.next;
 		}
 		this.end.data = data
-	}
+	};
+
+	this.addAtFirst = function(data) {
+		var temp = LinkList.makeNode();
+		temp.next = this.start;
+		this.start = temp;
+		temp.data = data;
+	};
+
+	this.delete = function(data) {
+		var current = this.start;
+		var previous = this.start;
+		while (current !== null) {
+			if (data === current.data) {
+				if (current === this.start) {
+					this.start = current.next;
+					return;
+				} 
+				if (current === this.end) {
+					this.end = previous;
+				}
+				previous.next = current.next;
+				return;
+			} else {
+				previous = current;
+				current = current.next;
+			}
+			
+		}
+	};
+
+	this.insertAfter = function(t, d) {
+		var current = this.start;
+		while(current !== null) {
+			if (current.data === t) {
+				var temp = LinkList.makeNode();
+				temp.data = d;
+				temp.next = current.next;
+				if (current == this.end) {
+					this.end = temp;
+				}
+				current.next = temp;
+				return;
+			}
+			current = current.next;
+		}
+	};
 
 }
